3c9b3d091585122270029965ba0addde8955412c
hamadazahera <hamadazahera@gmail.com>
2018-07-30 18:49:41 +0200

    added F1-score evaluation metric with random example.

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from Evaluation import Evaluation
 from Preprocessing import Preprocessing
 from secrets import consumer_key, consumer_secret, access_token, access_token_secret
 
@@ -13,3 +14,11 @@ training_Data = tweetsPrp.load_training_data()
 for _, tweet in training_Data.items():
     print(tweet.categories, tweet.indicatorTerms, tweet.text)
 
+# test evaluating of tweets classification with random y_true and y_predicted values:
+y_true = [1, 2, 4, 1, 5, 1, 1, 2, 1, 3, 3, 1, 1, 0, 5, 2]
+y_pred = [1, 3, 4, 3, 5, 1, 1, 3, 1, 3, 4, 1, 1, 2, 5, 2]
+
+eval = Evaluation(y_true, y_pred)
+
+print('Classification overall performance: F1 score', eval.f1_score)
+print('Classification accuracy: ', eval.accuracy_score)
