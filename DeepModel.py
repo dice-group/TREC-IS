7a25189ad64f78a33ac696cbfe12fa3ecaf09713
Hamada Zahera <hamadazahera@gmail.com>
2018-08-29 19:52:42 +0200

    Updated DeepModel

--- DeepModel.py
+++ DeepModel.py
@@ -1,7 +1,9 @@
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import scale, normalize
 
 
@@ -13,31 +15,44 @@ class Model:
         '''
         # Scale and normalize training features
         self.X = scale(X)
-        self.X = normalize(X, norm='l2')
+        self.X = normalize(X, norm='l2', axis=1)
 
         # encode y classes to one-hot
         self.y_encoded = to_categorical(y)
 
-        self.num_classes = 23  # number of classes (information types)
-        self.features_len = 301
+        self.num_classes = len(set(y))  # number of classes (information types)
+        self.features_len = len(X[0])  # number of features.
 
         self.X_train, self.x_test, self.y_train, self.y_test = train_test_split(self.X, self.y_encoded,
                                                                                 test_size=0.1)
+        # Feature Scaling
+        sc = StandardScaler()
+        self.X_train = sc.fit_transform(self.X_train)
+        self.x_test = sc.transform(self.x_test)
+
+        self.model = self.simple_DeepModel()
+
 
     def simple_DeepModel(self):
         model = Sequential()
-        model.add(Dense(32, activation='relu', input_dim=self.features_len))  # 301= 300 (embedding) + 1 (sentiment)
+        model.add(Dense(32, activation='relu', input_dim=self.features_len))
         model.add(Dense(self.num_classes, activation='softmax'))
         model.compile(optimizer='adam',
                       loss='categorical_crossentropy',
                       metrics=['accuracy'])
 
-        model.fit(self.X_train, self.y_train, epochs=30, batch_size=32, verbose=2, shuffle=True)
+        model.fit(self.X_train, self.y_train, epochs=100, batch_size=100, verbose=0, shuffle=True)
 
         return model
 
     def evaluate_model(self):
-        model = self.simple_DeepModel()
-        score = model.evaluate(self.x_test, self.y_test, batch_size=32, verbose=2)
+        score = self.model.evaluate(self.x_test, self.y_test, batch_size=100, verbose=0)
+        return score[1]
 
-        print(score[1])
+    def confusion_matrix(self):
+        y_pred = self.model.predict(self.x_test)
+        cm = confusion_matrix(self.y_test, y_pred)
+        return cm
+
+    def save_model(self, f_name):
+        self.model.save(f_name)
