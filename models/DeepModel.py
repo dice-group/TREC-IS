9a6d48e74a1a4df3cf95b1cbff8c618ef5d11798
Hamada Zahera <hamadazahera@gmail.com>
2018-10-29 19:11:22 +0100

    Update Deep Model-Categorical Accuracy

--- models/DeepModel.py
+++ models/DeepModel.py
@@ -1,27 +1,31 @@
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.utils import to_categorical
+from keras.metrics import  categorical_accuracy
+
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import scale, normalize
 
 
+
 class Model:
     def __init__(self, X, y):
         '''
-        :param X: training features
+        :param X: training extracted_Features
         :param y: categories
         '''
-        # Scale and normalize training features
-        self.X = scale(X)
+        # Scale and normalize training extracted_Features
+        # self.X = scale(X)
+
         self.X = normalize(X, norm='l2', axis=1)
 
         # encode y classes to one-hot
         self.y_encoded = to_categorical(y)
 
         self.num_classes = len(set(y))  # number of classes (information types)
-        self.features_len = len(X[0])  # number of features.
+        self.features_len = len(X[0])  # number of extracted_Features.
 
         self.X_train, self.x_test, self.y_train, self.y_test = train_test_split(self.X, self.y_encoded,
                                                                                 test_size=0.1)
@@ -39,7 +43,7 @@ class Model:
         model.add(Dense(self.num_classes, activation='softmax'))
         model.compile(optimizer='adam',
                       loss='categorical_crossentropy',
-                      metrics=['accuracy'])
+                      metrics=[categorical_accuracy])
 
         model.fit(self.X_train, self.y_train, epochs=100, batch_size=100, verbose=0, shuffle=True)
 
